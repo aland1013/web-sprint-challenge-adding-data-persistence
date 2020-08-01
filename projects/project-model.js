dcd9033e5667b0f55d060d7a5a041032ead15f49
aland1013 <adam.land@gmail.com>
2020-07-31 23:12:27 -0400

    created addProject helper function and POST /api/projects endpoint

--- projects/project-model.js
+++ projects/project-model.js
@@ -2,7 +2,9 @@ const db = require('../data/db-config');
 
 module.exports = {
   getProjects,
-  getResources
+  getResources,
+  getTasks,
+  addProject
 };
 
 function getProjects() {
@@ -11,4 +13,25 @@ function getProjects() {
 
 function getResources() {
   return db('resources');
+}
+
+function getTasks() {
+  return db('tasks')
+    .join('projects', 'tasks.projectId', 'projects.id')
+    .select(
+      'tasks.id',
+      { taskDesc: 'tasks.description' },
+      'tasks.notes',
+      'tasks.completed',
+      { projName: 'projects.name' },
+      { projDesc: 'projects.description' }
+    );
+}
+
+function addProject(newProject) {
+  return db('projects')
+    .insert(newProject)
+    .then((id) => {
+      return newProject;
+    });
 }
--- projects/project-router.js
+++ projects/project-router.js
@@ -26,4 +26,27 @@ router.get('/resources', (req, res) => {
     });
 });
 
+/* ----- GET /api/projects/tasks ----- */
+router.get('/tasks', (req, res) => {
+  Projects.getTasks()
+    .then((tasks) => {
+      res.json(tasks);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'Failed to get tasks' });
+    });
+});
+
+router.post('/', (req, res) => {
+  const newProject = req.body;
+
+  Projects.addProject(newProject)
+    .then((project) => {
+      res.status(201).json(project);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'Failed to create new project' });
+    });
+});
+
 module.exports = router;
